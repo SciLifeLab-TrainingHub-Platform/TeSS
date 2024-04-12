5acda46182a91d660df76a4d2a42a344b9460f3e
Mike Sanders <mikesndrs@gmail.com>
2024-04-12 11:35:56 +0200

    latest_materials on home page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -67,8 +67,26 @@ class StaticController < ApplicationController
       '',
       { 'start' => "#{Date.tomorrow.beginning_of_day}/" },
       sort_by: 'early',
-      per_page: 5 * n_events
-    ).results.group_by(&:content_provider_id).map { |_p_id, p_events| p_events.first }.first(n_events)
+      per_page: n_events
+    ).results
+    @materials = set_latest_materials
+  end
+
+  def showcase
+    @container_class = 'showcase-container container-fluid'
+  end
+
+  def set_latest_materials
+    n_materials = TeSS::Config.site.dig('home_page', 'latest_materials')
+    return [] unless n_materials
+
+    Materials.search_and_filter(
+      nil,
+      '',
+      { 'max_age' => '1 month' },
+      sort_by: 'new',
+      per_page: 10 * n_materials
+    ).results.group_by(&:content_provider_id).map { |_p_id, p_materials| p_materials.first }.first(n_materials)
   end
 
   def set_count_strings
