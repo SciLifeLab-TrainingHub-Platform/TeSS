4687ee0669f7d666ac57a58c983a7479c570e5a7
Mike Sanders <mikesndrs@gmail.com>
2024-04-24 16:08:55 +0200

    catalogue blocks counts

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -28,7 +28,8 @@ class StaticController < ApplicationController
   end
 
     @events = set_upcoming_events
-    @featured_trainer = set_featured_trainer
+    @materials = set_latest_materials
+    @catalogue_count_strings = set_catalogue_count_strings
   end
 
   def showcase
@@ -129,5 +130,33 @@ class StaticController < ApplicationController
     count_strings['content_providers'] = ContentProvider.all.count
     count_strings['trainers'] = Trainer.all.count
     count_strings
+  end
+
+  def set_upcoming_events
+    n_events = TeSS::Config.site.dig('home_page', 'upcoming_events')
+    return [] unless n_events
+
+    Event.search_and_filter(
+      nil,
+      '',
+      { 'start' => "#{Date.tomorrow.beginning_of_day}/" },
+      sort_by: 'early',
+      per_page: 5 * n_events
+    ).results.group_by(&:content_provider_id).map { |_p_id, p_events| p_events.first }.first(n_events)
+  end
+
+  def set_catalogue_count_strings
+    catalogue_count_strings = {}
+    return catalogue_count_strings unless TeSS::Config.site.dig('home_page', 'catalogue_counts')
+
+    total_events = Event.pluck(:end).select { |x| x >= Time.zone.now }.count
+    total_events = "#{total_events.round(-2).to_f / 1000}k" if total_events > 100
+    last_month_events = Event.pluck(:created_at).select { |x| x >= Time.zone.now - 1.month }.count
+    catalogue_count_strings['events'] = t('home.catalogue.events_count', total_events:, last_month_events:)
+    catalogue_count_strings['materials'] = t('home.catalogue.materials_count', total_materials: Material.all.count)
+    catalogue_count_strings['workflows'] = t('home.catalogue.workflows_count', total_workflows: Workflow.all.count)
+    catalogue_count_strings['content_providers'] = t('home.catalogue.content_providers_count', total_content_providers: ContentProvider.all.count)
+    catalogue_count_strings['trainers'] = t('home.catalogue.trainers_count', total_trainers: Trainer.all.count)
+    catalogue_count_strings
   end
 end
