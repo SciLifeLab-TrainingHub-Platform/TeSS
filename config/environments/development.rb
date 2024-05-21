e5712427f538b6507eb48e46e4014ba6d8758f76
harshita3199 <harshita.gupta@scilifelab.se>
2024-05-21 18:09:55 +0200

    update the dev host
    Signed-off-by: harshita3199 <harshita.gupta@scilifelab.se>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts = [
-    "localhost",                    # The localhost reserved domain.
-    ENV["RAILS_DEVELOPMENT_HOSTS"]  
+  config.hosts << ENV["RAILS_DEVELOPMENT_HOSTS"]  
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
