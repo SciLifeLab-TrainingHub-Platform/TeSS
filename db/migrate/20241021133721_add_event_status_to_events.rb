42955a7fcd0c39432873027cc1c6880989ab2e33
Adeel Ahmad <adeelahmaddev@gmail.com>
2024-10-25 09:26:56 +0200

    #187 | Send the event request to admin to approve and reject through … (#194)
    Co-authored-by: Adeel Ahmad <adeel@client72-229.fysbot.umu.se>

---
+++ db/migrate/20241021133721_add_event_status_to_events.rb
@@ -0,0 +1,5 @@
+class AddEventStatusToEvents < ActiveRecord::Migration[7.0]
+  def change
+    add_column :events, :event_status, :integer, default: 0, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_02_112549) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_21_133721) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -269,6 +269,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_02_112549) do
     t.boolean "visible", default: true
     t.string "language"
     t.datetime "application_deadline"
+    t.integer "event_status", default: 0, null: false
     t.index ["presence"], name: "index_events_on_presence"
     t.index ["slug"], name: "index_events_on_slug", unique: true
     t.index ["user_id"], name: "index_events_on_user_id"
