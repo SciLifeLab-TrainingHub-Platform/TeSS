2b880c3b2eebb403550605cbb9b7269df23112ce
Harshita Gupta <39816522+harshita3199@users.noreply.github.com>
2025-01-27 14:51:19 +0100

    hotfix-z228-Resolve the issue of db in production (#229)
    Co-authored-by: Finn Bacall <finn.bacall@manchester.ac.uk>

--- db/schema.rb
+++ db/schema.rb
@@ -457,15 +457,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_04_121716) do
     t.string "title"
   end
 
-  create_table "sessions", force: :cascade do |t|
-    t.string "session_id", null: false
-    t.text "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
-    t.index ["updated_at"], name: "index_sessions_on_updated_at"
-  end
-
   create_table "sources", force: :cascade do |t|
     t.bigint "content_provider_id"
     t.bigint "user_id"
