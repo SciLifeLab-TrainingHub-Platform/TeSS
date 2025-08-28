5689d3c39235c27a7fa031f584cddd33143da10a
Harshita Gupta <39816522+harshita3199@users.noreply.github.com>
2025-08-28 12:47:44 +0200

    Clean up blank lines in db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-
-
 ActiveRecord::Schema[7.0].define(version: 2025_07_23_130821) do 
 
   # These are extensions that must be enabled in order to support this database
