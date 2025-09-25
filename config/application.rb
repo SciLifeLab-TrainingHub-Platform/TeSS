fda9f8ab13ea7dd331f5f686baeb58052a2a1d61
Harshita Gupta <harshita.gupta@scilifelab.uu.se>
2025-09-25 10:59:39 +0200

    remove duplicate logic
    Signed-off-by: Harshita Gupta <harshita.gupta@scilifelab.uu.se>

--- config/application.rb
+++ config/application.rb
@@ -87,8 +87,7 @@ module TeSS
     end
 
     def analytics_enabled
-      force_analytics_enabled || 
-      (Rails.application.secrets.google_analytics_code.present? && Rails.env.production?) ||
+      force_analytics_enabled || (Rails.application.secrets.google_analytics_code.present? && Rails.env.production?) ||
       (Rails.application.secrets.matomo_url.present? && ENV['DEPLOYMENT_ENV'] == 'live')
     end
 
