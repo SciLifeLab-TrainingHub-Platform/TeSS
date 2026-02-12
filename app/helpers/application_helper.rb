82b5fe1d2bf56839096f49054f2f7ea722d3f50a
Adeel Ahmad <adeelahmaddev@gmail.com>
2026-02-12 14:29:28 +0100

    Remove green tick icon for approved courses and events (#395)
    Co-authored-by: Adeel Ahmad <adeel@client72-229.fysbot.umu.se>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -106,7 +106,7 @@ module ApplicationHelper
                                                 when 'awaiting_review'
                                                   ['fa-clock-o', 'awaiting-review', 'Pending review by SciLifeLab administrators.']
                                                 when 'approved'
-                                                  ['fa-check', 'approved', 'Approved by SciLifeLab and now publicly accessible.']
+                                                  return # Approved items now do not show a status badge
                                                 when 'declined'
                                                   ['fa-ban', 'declined', 'Declined by SciLifeLab administrators.']
                                                 when 'revisions_required'
