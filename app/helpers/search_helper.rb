7c2dcb283e768ca1751dd13e5693f53ff4183ae0
Adeel Ahmad <adeel@client72-229.fysbot.umu.se>
2026-02-16 16:33:00 +0100

    renamed course catalogue to training catalogue

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -18,14 +18,14 @@ module SearchHelper
   def filter_link(name, value, count, html_options = {}, &block)
     parameters = search_and_facet_params
 
-    #if there's already a filter of the same facet type, create/add to an array
+    # if there's already a filter of the same facet type, create/add to an array
     if parameters.include?(name) && !html_options.delete(:replace)
       parameters[name] = Array.wrap(parameters[name]) | [value]
     else
       parameters[name] = value
     end
 
-    parameters.delete('page') #remove the page option if it exists
+    parameters.delete('page') # remove the page option if it exists
     html_options.reverse_merge!(title: value.to_s)
 
     link_to parameters, html_options do
@@ -41,7 +41,7 @@ module SearchHelper
   def remove_filter_link(name, value, html_options = {}, &block)
     parameters = search_and_facet_params
 
-    #delete a filter from an array or delete the whole facet if it is the only one
+    # delete a filter from an array or delete the whole facet if it is the only one
     if parameters.include?(name)
       if parameters[name].is_a?(Array)
         parameters[name].delete(value)
@@ -52,7 +52,7 @@ module SearchHelper
       end
     end
 
-    parameters.delete('page') #remove the page option if it exists
+    parameters.delete('page') # remove the page option if it exists
     html_options.reverse_merge!(title: value.to_s)
 
     link_to parameters, html_options do
@@ -73,5 +73,34 @@ module SearchHelper
             Show fewer #{facet.humanize.pluralize.downcase}</span>
             <i class='glyphicon glyphicon-chevron-up pull-right toggle-#{facet}' style='display: none;'></i>
             ".html_safe
+  end
+
+  def searchable_resource_name(resource_type, variant: :short, count: 2)
+    model_key = resource_type.model_name.i18n_key
+    i18n_key  = :"features.#{model_key.to_s.pluralize}.#{variant}"
+
+    name =
+      if I18n.exists?(i18n_key)
+        I18n.t(i18n_key)
+      else
+        resource_type.model_name.human(count: count)
+      end
+    name.downcase
+  end
+
+  # Returns a count string with custom names if model is in custom_model_entries
+  def search_result_label(count, resource_type, variant: :short)
+    custom_model_entries = {
+      course: "catalogue entry"
+    }
+
+    model_key = resource_type.model_name.i18n_key
+
+    # Use custom entry if defined, otherwise fallback to default
+    if custom_model_entries.key?(model_key)
+      pluralize(count, custom_model_entries[model_key])
+    else
+      pluralize(count, resource_type.model_name.human.downcase)
+    end
   end
 end
