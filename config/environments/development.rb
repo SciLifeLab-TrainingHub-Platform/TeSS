d29cef57a21b329f5951efcf8231167566aac4d4
Harshita Gupta <harshita.gupta@scilifelab.uu.se>
2026-04-22 16:22:14 +0200

    modify the hos again
    Signed-off-by: Harshita Gupta <harshita.gupta@scilifelab.uu.se>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # KTH / SciLifeLab dev gateways (e.g. th-dev.gw.scilifelab-2-dev.sys.kth.se)
-  #config.hosts << "th-dev.gw.scilifelab-2-dev.sys.kth.se"
+  # Allow requests for scilifelab-2-dev.sys.kth.se and any subdomain
+  # (e.g. th-dev.gw.scilifelab-2-dev.sys.kth.se). Same pattern as config.hosts << ".product.com".
+  config.hosts << ".scilifelab-2-dev.sys.kth.se"
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
