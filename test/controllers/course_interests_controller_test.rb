2cd775466a3a3ee418ae7489c5ffb2eeb479bfa8
Adeel Ahmad <adeel@client72-229.fysbot.umu.se>
2026-06-17 16:20:08 +0200

    #317 | added test for CourseSubscriptionsControllerTest and CourseInterestsControllerTest

--- test/controllers/course_interests_controller_test.rb
+++ test/controllers/course_interests_controller_test.rb
@@ -1,33 +1,147 @@
 require "test_helper"
 
-class CourseInterestsControllerTest < ActionDispatch::IntegrationTest
-  test "should get subscribe" do
-    get course_interests_subscribe_url
-    assert_response :success
+class CourseInterestsControllerTest  < ActionController::TestCase
+  include Devise::Test::ControllerHelpers
+
+  setup do
+    @user = users(:regular_user)
+    sign_in @user
+
+    @course = courses(:one)
   end
 
-  test "should get unsubscribe" do
-    get course_interests_unsubscribe_url
-    assert_response :success
+  # test for non logged in users
+  test "should redirect with alert when user is not logged in" do
+    sign_out @user
+    post :create, params: { course_id: @course.to_param }
+    assert_redirected_to new_user_session_path
   end
 
-  test "should get subscribe_as_guest" do
-    get course_interests_subscribe_as_guest_url
-    assert_response :success
+  # Successful subscribe
+  test "should subscribe user and redirect with notice" do
+    service_stub = lambda do |course:, user:|
+      assert_equal @course, course
+      assert_equal @user, user
+      {
+        status: :success,
+        message: "You have subscribed successfully"
+      }
+    end
+    CourseInterestService.stub(:subscribe_user!, service_stub) do
+      post :create, params: { course_id: @course.to_param }
+    end
+    assert_redirected_to course_path(@course)
+    assert_equal "You have subscribed successfully", flash[:notice]
   end
 
-  test "should get request_unsubscribe_email" do
-    get course_interests_request_unsubscribe_email_url
-    assert_response :success
+  # unsuccessful subscribe
+  test "should redirect with alert when subscription fails" do
+    service_stub = lambda do |course:, user:|
+      assert_equal @course, course
+      assert_equal @user, user
+      {
+        status: :error,
+        message: "Already subscribed."
+      }
+    end
+
+    CourseInterestService.stub(:subscribe_user!, service_stub) do
+      post :create, params: { course_id: @course.to_param }
+    end
+
+    assert_redirected_to course_path(@course)
+    assert_equal "Already subscribed.", flash[:alert]
   end
 
-  test "should get confirm_subscription" do
-    get course_interests_confirm_subscription_url
-    assert_response :success
+
+  # course not found
+  test "should redirect to courses path when course is not found" do
+    post :create, params: { course_id: "invalid-course" }
+
+    assert_redirected_to courses_path
+    assert_equal "Course not found", flash[:alert]
   end
 
-  test "should get confirm_unsubscribe" do
-    get course_interests_confirm_unsubscribe_url
-    assert_response :success
+  # generic error
+  test "should redirect with generic error when exception occurs" do
+    service_stub = lambda do |course:, user:|
+      raise StandardError
+    end
+
+    CourseInterestService.stub(:subscribe_user!, service_stub) do
+      post :create, params: { course_id: @course.to_param }
+    end
+
+    assert_redirected_to course_path(@course)
+    assert_equal "Something went wrong. Please try again.", flash[:alert]
+  end
+
+  # successful unsubscribe
+  test "should unsubscribe user and redirect with notice" do
+    service_stub = lambda do |course:, user:|
+      assert_equal @course, course
+      assert_equal @user, user
+
+      {
+        status: :success,
+        message: "You have unsubscribed successfully"
+      }
+    end
+
+    CourseInterestService.stub(:unsubscribe_user!, service_stub) do
+      delete :destroy, params: { course_id: @course.to_param }
+    end
+
+    assert_redirected_to course_path(@course)
+    assert_equal "You have unsubscribed successfully", flash[:notice]
+  end
+
+  # unsuccessful unsubscribe
+  test "should redirect with alert when unsubscription fails" do
+    service_stub = lambda do |course:, user:|
+      assert_equal @course, course
+      assert_equal @user, user
+
+      {
+        status: :error,
+        message: "You are not currently subscribed"
+      }
+    end
+
+    CourseInterestService.stub(:unsubscribe_user!, service_stub) do
+      delete :destroy, params: { course_id: @course.to_param }
+    end
+
+    assert_redirected_to course_path(@course)
+    assert_equal "You are not currently subscribed", flash[:alert]
+  end
+
+  # test for non logged in users in destroy
+  test "should redirect to sign in when user is not logged in" do
+    sign_out @user
+    delete :destroy, params: { course_id: @course.to_param }
+    assert_redirected_to new_user_session_path
+  end
+
+  # Course not found
+  test "should redirect to courses path when course is not found in destroy" do
+    delete :destroy, params: { course_id: "invalid-course" }
+
+    assert_redirected_to courses_path
+    assert_equal "Course not found", flash[:alert]
+  end
+
+  # generic error in destroy
+  test "should redirect with generic error when exception occurs in destroy" do
+    service_stub = lambda do |course:, user:|
+      raise StandardError
+    end
+
+    CourseInterestService.stub(:unsubscribe_user!, service_stub) do
+      delete :destroy, params: { course_id: @course.to_param }
+    end
+
+    assert_redirected_to course_path(@course)
+    assert_equal "Something went wrong. Please try again.", flash[:alert]
   end
 end
--- test/controllers/course_subscriptions_controller_test.rb
+++ test/controllers/course_subscriptions_controller_test.rb
@@ -1,7 +1,375 @@
 require "test_helper"
 
-class CourseSubscriptionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class CourseSubscriptionsControllerTest < ActionController::TestCase
+  include Devise::Test::ControllerHelpers
+
+  setup do
+    @user = users(:regular_user)
+    @course = courses(:one)
+    @interest = course_interests(:one)
+  end
+
+  # tests for request_email_action method
+  test "request_email_action should redirect logged in user to root path" do
+    sign_in @user
+
+    post :request_email_action,
+         params: {
+           course_id: @course.to_param,
+           email: "test@example.com",
+           action_type: CourseInterest::ACTION_REQUEST_SUBSCRIBE
+         }
+
+    assert_redirected_to root_path
+    assert_equal(
+      "Logged-in users should manage subscriptions through their account.",
+      flash[:alert]
+    )
+  end
+
+  test "request_email_action should request subscription and redirect with notice" do
+    sign_out @user
+
+    service_stub = lambda do |course:, email:|
+      assert_equal @course, course
+      assert_equal "test@example.com", email
+
+      {
+        status: :ok,
+        message: "Subscription confirmation email sent"
+      }
+    end
+
+    CourseInterestService.stub(:request_subscription!, service_stub) do
+      post :request_email_action,
+           params: {
+             course_id: @course.to_param,
+             email: "test@example.com",
+             action_type: CourseInterest::ACTION_REQUEST_SUBSCRIBE
+           }
+    end
+
+    assert_redirected_to course_path(@course)
+    assert_equal "Subscription confirmation email sent", flash[:notice]
+  end
+
+  test "request_email_action should redirect with alert when subscription request fails" do
+    sign_out @user
+
+    CourseInterestService.stub(
+      :request_subscription!,
+      {
+        status: :error,
+        message: "Invalid request"
+      }
+    ) do
+      post :request_email_action,
+           params: {
+             course_id: @course.to_param,
+             email: "test@example.com",
+             action_type: CourseInterest::ACTION_REQUEST_SUBSCRIBE
+           }
+    end
+
+    assert_redirected_to course_path(@course)
+    assert_equal "Invalid request", flash[:alert]
+  end
+
+  test "request_email_action should request unsubscription and redirect with notice" do
+    sign_out @user
+
+    service_stub = lambda do |course:, email:|
+      assert_equal @course, course
+      assert_equal "test@example.com", email
+
+      {
+        status: :ok,
+        message: "Unsubscription confirmation email sent"
+      }
+    end
+
+    CourseInterestService.stub(:request_unsubscription!, service_stub) do
+      post :request_email_action,
+           params: {
+             course_id: @course.to_param,
+             email: "test@example.com",
+             action_type: CourseInterest::ACTION_REQUEST_UNSUBSCRIBE
+           }
+    end
+
+    assert_redirected_to course_path(@course)
+    assert_equal "Unsubscription confirmation email sent", flash[:notice]
+  end
+
+  test "request_email_action should redirect with alert for invalid action type" do
+    sign_out @user
+
+    post :request_email_action,
+         params: {
+           course_id: @course.to_param,
+           email: "test@example.com",
+           action_type: "invalid_action"
+         }
+
+    assert_redirected_to course_path(@course)
+    assert_equal "Invalid action", flash[:alert]
+  end
+
+  test "request_email_action should redirect to courses path when course is not found" do
+    post :request_email_action,
+         params: {
+           course_id: "invalid-course",
+           email: "test@example.com",
+           action_type: CourseInterest::ACTION_REQUEST_SUBSCRIBE
+         }
+
+    assert_redirected_to courses_path
+    assert_equal "Course not found", flash[:alert]
+  end
+
+  test "request_email_action should redirect with generic error when exception occurs" do
+    sign_out @user
+
+    service_stub = lambda do |course:, email:|
+      raise StandardError
+    end
+
+    CourseInterestService.stub(:request_subscription!, service_stub) do
+      post :request_email_action,
+           params: {
+             course_id: @course.to_param,
+             email: "test@example.com",
+             action_type: CourseInterest::ACTION_REQUEST_SUBSCRIBE
+           }
+    end
+
+    assert_redirected_to course_path(@course)
+    assert_equal "Something went wrong. Please try again.", flash[:alert]
+  end
+
+  # tests for confirm_course_subscription method
+  test "confirm_course_subscription redirects with notice when subscription is confirmed" do
+
+    interest = course_interests(:one)
+    service_stub = lambda do |interest:|
+      assert_equal course_interests(:one), interest
+      {
+        status: :ok,
+        message: "Subscription confirmed successfully"
+      }
+    end
+    CourseInterest.stub(:find_signed!, interest) do
+      CourseInterestService.stub(:confirm_subscription!, service_stub) do
+        get :confirm_course_subscription,
+            params: {
+              course_id: @course.to_param,
+              token: "valid-token"
+            }
+      end
+    end
+    assert_redirected_to course_path(@course)
+    assert_equal "Subscription confirmed successfully", flash[:notice]
+  end
+
+  test "confirm_course_subscription redirects with alert when service returns error" do
+    interest = course_interests(:one)
+
+    service_stub = lambda do |interest:|
+      assert_equal course_interests(:one), interest
+
+      {
+        status: :error,
+        message: "Invalid subscription link"
+      }
+    end
+
+    CourseInterest.stub(:find_signed!, interest) do
+      CourseInterestService.stub(:confirm_subscription!, service_stub) do
+        get :confirm_course_subscription,
+            params: {
+              course_id: @course.to_param,
+              token: "valid-token"
+            }
+      end
+    end
+
+    assert_redirected_to course_path(@course)
+    assert_equal "Invalid subscription link", flash[:alert]
+  end
+
+  test "confirm_course_subscription redirects with alert when token course does not match" do
+    interest = course_interests(:one)
+
+    interest.stub(:course_id, courses(:two).id) do
+      CourseInterest.stub(:find_signed!, interest) do
+        get :confirm_course_subscription,
+            params: {
+              course_id: @course.to_param,
+              token: "valid-token"
+            }
+      end
+    end
+
+    assert_redirected_to course_path(@course)
+    assert_equal "Invalid confirmation link", flash[:alert]
+  end
+
+  test "confirm_course_subscription redirects with alert when token is invalid" do
+    CourseInterest.stub(
+      :find_signed!,
+      ->(*) { raise ActiveSupport::MessageVerifier::InvalidSignature }
+    ) do
+      get :confirm_course_subscription,
+          params: {
+            course_id: @course.to_param,
+            token: "invalid-token"
+          }
+    end
+
+    assert_redirected_to course_path(@course)
+    assert_equal "Invalid or expired confirmation link", flash[:alert]
+  end
+
+  test "confirm_course_subscription redirects to courses path when course is not found" do
+    get :confirm_course_subscription,
+        params: {
+          course_id: "invalid-course",
+          token: "valid-token"
+        }
+
+    assert_redirected_to courses_path
+    assert_equal "Course not found", flash[:alert]
+  end
+
+  test "confirm_course_subscription redirects with alert when unexpected error occurs" do
+    interest = course_interests(:one)
+
+    CourseInterest.stub(:find_signed!, interest) do
+      CourseInterestService.stub(
+        :confirm_subscription!,
+        ->(*) { raise StandardError }
+      ) do
+        get :confirm_course_subscription,
+            params: {
+              course_id: @course.to_param,
+              token: "valid-token"
+            }
+      end
+    end
+
+    assert_redirected_to course_path(@course)
+    assert_equal "Something went wrong. Please try again.", flash[:alert]
+  end
+
+  # tests for confirm_course_unsubscription method
+  test "confirm_course_unsubscription redirects with notice when unsubscription is confirmed" do
+    interest = course_interests(:one)
+
+    service_stub = lambda do |interest|
+      assert_equal course_interests(:one), interest
+      {
+        status: :ok,
+        message: "You have been unsubscribed successfully"
+      }
+    end
+
+    CourseInterest.stub(:find_signed!, interest) do
+      CourseInterestService.stub(:confirm_unsubscription, service_stub) do
+        get :confirm_course_unsubscription,
+            params: {
+              course_id: @course.to_param,
+              token: "valid-token"
+            }
+      end
+    end
+
+    assert_redirected_to course_path(@course)
+    assert_equal "You have been unsubscribed successfully", flash[:notice]
+  end
+
+  test "confirm_course_unsubscription redirects with alert when service returns error" do
+    interest = course_interests(:one)
+
+    service_stub = lambda do |interest|
+      {
+        status: :error,
+        message: "Invalid unsubscription link"
+      }
+    end
+
+    CourseInterest.stub(:find_signed!, interest) do
+      CourseInterestService.stub(:confirm_unsubscription, service_stub) do
+        get :confirm_course_unsubscription,
+            params: {
+              course_id: @course.to_param,
+              token: "valid-token"
+            }
+      end
+    end
+
+    assert_redirected_to course_path(@course)
+    assert_equal "Invalid unsubscription link", flash[:alert]
+  end
+
+  test "confirm_course_unsubscription redirects with alert when token course does not match" do
+    interest = Struct.new(:course_id).new(-1)
+
+    CourseInterest.stub(:find_signed!, interest) do
+      get :confirm_course_unsubscription,
+          params: {
+            course_id: @course.to_param,
+            token: "valid-token"
+          }
+    end
+
+    assert_redirected_to course_path(@course)
+    assert_equal "Invalid confirmation link", flash[:alert]
+  end
+
+  test "confirm_course_unsubscription redirects with alert when token is invalid" do
+    CourseInterest.stub(
+      :find_signed!,
+      ->(*) { raise ActiveSupport::MessageVerifier::InvalidSignature }
+    ) do
+      get :confirm_course_unsubscription,
+          params: {
+            course_id: @course.to_param,
+            token: "invalid-token"
+          }
+    end
+
+    assert_redirected_to course_path(@course)
+    assert_equal "Invalid or expired confirmation link", flash[:alert]
+  end
+
+  test "confirm_course_unsubscription redirects to courses path when course is not found" do
+    get :confirm_course_unsubscription,
+        params: {
+          course_id: "invalid-course",
+          token: "valid-token"
+        }
+
+    assert_redirected_to courses_path
+    assert_equal "Course not found", flash[:alert]
+  end
+
+  test "confirm_course_unsubscription redirects with alert when unexpected error occurs" do
+    interest = course_interests(:one)
+
+    CourseInterest.stub(:find_signed!, interest) do
+      CourseInterestService.stub(
+        :confirm_unsubscription,
+        ->(*) { raise StandardError }
+      ) do
+        get :confirm_course_unsubscription,
+            params: {
+              course_id: @course.to_param,
+              token: "valid-token"
+            }
+      end
+    end
+
+    assert_redirected_to course_path(@course)
+    assert_equal "Something went wrong. Please try again.", flash[:alert]
+  end
 end
