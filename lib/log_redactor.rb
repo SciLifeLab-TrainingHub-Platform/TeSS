ef32899237ebec9fe80e0fbad97ccc0cba1d2606
Adeel Ahmad <adeel@client72-229.fysbot.umu.se>
2026-07-20 13:57:29 +0200

    #317 | fix lib/log_redactor.rb based on coderabbit suggestion

--- lib/log_redactor.rb
+++ lib/log_redactor.rb
@@ -3,10 +3,14 @@
 module LogRedactor
   module_function
 
+  CONTROL_CHARS = /[\x00-\x1f\x7f]/
+
   def email_log_id(email)
     return nil if email.blank?
     user, domain = email.split("@", 2)
     return "[redacted]" if domain.blank?
-    "#{user[0]}***@#{domain}"
+    first_char = user[0].to_s.gsub(CONTROL_CHARS, "")
+    safe_domain = domain.gsub(CONTROL_CHARS, "")
+    "#{first_char}***@#{safe_domain}"
   end
 end
