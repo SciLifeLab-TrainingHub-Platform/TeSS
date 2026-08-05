5071be8398c1a3051c87116e9d18047636453f2a
Adeel Ahmad <adeel@Adeels-MacBook-Pro.local>
2026-08-05 16:20:26 +0200

    fix the code review remarks

--- app/controllers/course_subscriptions_controller.rb
+++ app/controllers/course_subscriptions_controller.rb
@@ -15,6 +15,10 @@ class CourseSubscriptionsController < ApplicationController
       return redirect_to course_path(course)
     end
 
+    pp("in request_email_action")
+    pp("params[:action_type]")
+    pp(params[:action_type])
+
     result =
       case params[:action_type]
       when CourseInterest::ACTION_REQUEST_SUBSCRIBE
@@ -24,6 +28,9 @@ class CourseSubscriptionsController < ApplicationController
       else
         return redirect_to course_path(course), alert: "Invalid action"
       end
+
+    pp('result this this as a')
+    pp(result)
 
     if result[:status] == :error
       redirect_to course_path(course), alert: result[:message]
--- app/controllers/tess_devise/registrations_controller.rb
+++ app/controllers/tess_devise/registrations_controller.rb
@@ -26,7 +26,7 @@ class TessDevise::RegistrationsController < Devise::RegistrationsController
   # Pinched from https://github.com/plataformatec/devise/wiki/How-To:-Use-Recaptcha-with-Devise
   def check_captcha
     return if Rails.application.secrets.recaptcha[:sitekey].blank?
-    recaptcha_valid = verify_recaptcha(action: 'signup', minimum_score: 0.5)
+    recaptcha_valid = verify_recaptcha(response: params[:recaptcha_token], action: 'signup', minimum_score: 0.5)
     unless recaptcha_valid
       self.resource = resource_class.new sign_up_params
       respond_with_navigational(resource) { render :new }
